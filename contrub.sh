0fd8392389562186ff8cf36886fb19580683a718
= <=>
2024-03-07 18:20:32 +0100

    commit number {0..5}

--- contrub.sh
+++ contrub.sh
@@ -1,1 +1,1 @@
-for i in {0..50};do echo "# $i" >> setup.sh && git add . && git commit -m "commit number $i" ;if [[ $((i%1000)) == 0 ]];then git push ;fi ;done
+for i in {0.."$1"};do echo "# $i" >> setup.sh && git add . && git commit -m "commit number $i" ;if [[ $((i%1000)) == 0 ]];then git push ;fi ;done
